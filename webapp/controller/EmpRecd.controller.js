b6a07da7dfe57fe964554ebf1214194b6d0911bb
ashwiniaher <ashwini.p.aher@accenture.com>
2017-06-13 10:55:30 +0000

    Table add row demo

--- webapp/controller/EmpRecd.controller.js
+++ webapp/controller/EmpRecd.controller.js
@@ -3,40 +3,22 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 	// Model Data (local) 
-	 var tableData = [{Sr: "1", fname: "",   lname: "", empid: "", city:""}];
+	 var tableData = [{sr: "1", fname: "",   lname: "", empid: "", city:""}];
 	 
 	return Controller.extend("TestGit.controller.EmpRecd", {
-		 
-		updateData : function(){
-			tableData.push({Sr: "1", fname: "",   lname: "", empid: "", city:""});
-		},
-		addRow : function(){
+		onInit: function(){
 			//Create a model and bind the table rows to this model  
             var oModel = new sap.ui.model.json.JSONModel(); // created a JSON model        
             oModel.setData({ // Set the data to the model using the JSON object defined already  
                 modelData: tableData  
             });  
             this.getView().setModel(oModel);
+		},
+		addRow : function(){
             var table = sap.ui.getCore().byId(this.createId("EmpRcdTable"));     
-            
-            var listItems = new sap.m.ColumnListItem({
-				cells: [
-					new sap.m.Text({
-						text:this.getRowCount(table)+1
-					}),
-					new sap.m.Input({
-						value: "{Fname}"
-					}), new sap.m.Input({
-						value: "{Lname}"
-					}), new sap.m.Input({
-						value: "{EmpId}"
-					}), new sap.m.Input({
-						value: "{City}"
-					})
-				]
-			});
-            table.bindItems("/modelData",listItems);
-            this.updateData();
+			var data = this.getView().getModel().getProperty("/modelData");
+			data.push({sr: this.getRowCount(table)+1, fname: "",   lname: "", empid: "", city:""});
+			this.getView().getModel().setProperty("/modelData",data);
 		},
 		getRowCount: function(table){
 			//get binding path of table items
@@ -47,17 +29,6 @@ sap.ui.define([
 			var data = model.getProperty(bindingPath);
 			//get total rows number
 			return data.length;
-		},
-		onSave : function(oEvent){
-			/////////////////
-			
-			var oTable = sap.ui.getCore().byId(this.createId("EmpRcdTable"));
-			
-			var oModel = sap.ui.getCore().getModel().getProperty("/EmpRcdData");
-			oModel.push({Product: "New Product", Weight: "45"});
-	        sap.ui.getCore().getModel().setProperty("/EmpRcdData", oModel);
-			/////////////////
-			
 		}
 		
 	});
